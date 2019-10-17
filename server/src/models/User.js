848ebcf6a9ba715b830c83118bf6676e96121708
Bram <brampijper@gmail.com>
2019-10-17 22:57:58 +0200

    Implemented crypto, to hash the user's password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,36 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define('User', {
+const crypto = require('crypto');
+
+function storePassword(user, options) {
+    if (!user.changed('password')) {
+        return;
+    }
+    return user.setDataValue('password', hashedPassword(user.password))
+}
+
+function hashedPassword(password) {
+    return crypto
+        .pbkdf2Sync(password, '8', 100000, 64, 'sha512')
+        .toString('hex');
+}
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
         email: {
             type: DataTypes.STRING,
             unique: true
         },
         password: DataTypes.STRING
+    },
+    {
+        hooks: {
+            beforeSave: storePassword,
+            beforeUpdate: storePassword,
+            beforeSave: storePassword,
+        }
     })
+
+    User.prototype.comparePassword = function (password) {
+        return hashedPassword(password) === this.password
+    }
+    return User
+}
