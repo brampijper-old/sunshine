da561e74a999c65f3254fce45f68392a9c3cd1a8
Bram <brampijper@gmail.com>
2019-10-17 22:52:45 +0200

    Created a settings component where the user can change his settings in the future

---
+++ client/src/components/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <span v-if="this.$store.state.message" class="message">
+            {{this.$store.state.message}}
+        </span>
+        <br>
+        <br>
+        <h3> Change user settings </h3>
+        <h3> See how many notifications a user received </h3>
+        <h3> Delete your account </h3>
+    </div>
+</template>
+
+<style scoped>
+span {
+    color: #3EAF7C;
+}
+</style>
