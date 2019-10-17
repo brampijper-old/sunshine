96b980e72ef4d66cdafa4ddbf408889339262271
Bram <brampijper@gmail.com>
2019-10-17 14:33:59 +0200

    added a route for the login page

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/HelloWorld.vue'
 import Register from './components/Register.vue'
+import Login from './components/Login.vue'
 
 Vue.use(Router)
 
@@ -10,12 +10,16 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
     },
     {
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
