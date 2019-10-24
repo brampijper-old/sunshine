0927d0f724241eff9c15f4e75b03922ee54a60e3
Bram <brampijper@gmail.com>
2019-10-24 20:48:44 +0200

    Implemented debounce function for limiting api calls when user is typing

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -3,11 +3,12 @@
         <div class="container__left">
             <h1 class="left__title"> Sign Up </h1>
 
-            <form name="sunshine-register-form" autocomplete="off">
+            <form name="sunshine-register-form">
                 <label for="email">Email adress</label>
                 <input 
                     type="text"
                     name="email"
+                    autocomplete="email"
                     v-model="email"
                     placeholder="Enter email adress" />
 
@@ -16,7 +17,28 @@
                     type="password"
                     name="password"
                     v-model="password"
+                    autocomplete="new-password"
                     placeholder="Password" />
+
+                <label for="location">City</label>
+                <input
+                    id="location__input"
+                    name="location"
+                    v-model="adress.city"
+                    @input="debounceApiCall()"
+                    placeholder="City where you live / work"
+                    autocomplete="adress-level2" />
+                <ul class="suggestion__container">
+                    <a
+                        @click="adress = loc, adressAutocomplete=[]"
+                        href="#"
+                        v-for="(loc, index) in adressAutocomplete"
+                        :key="index">
+                        <li>
+                            {{loc.city}}
+                        </li>
+                    </a>
+                </ul>
             </form>
             
             <div class="error" v-html="error" />
@@ -45,7 +67,9 @@
 <script>
 import AuthenticationService from '@/services/AuthenticationService';
 import PrimaryButton from './PrimaryButton.vue';
-import { navigateToMixin } from '../mixins/navigateToMixin.js'
+import { navigateToMixin } from '../mixins/navigateToMixin.js';
+import { MakeHTTPRequest } from '../mixins/MakeHTTPRequest.js';
+
 export default {
     mixins: [navigateToMixin],
     components: {
@@ -55,7 +79,10 @@ export default {
         return {
             email: '',
             password: '',
-            error: null
+            adress: [{city: null, lat: null, lng: null}],
+            adressAutocomplete: [],
+            error: null,
+            timer: null
         }
     },
     methods: {
@@ -63,20 +90,49 @@ export default {
             try {
                 const response = await AuthenticationService.register({
                     email: this.email,
-                    password: this.password
+                    password: this.password,
+                    adress: this.adress
                 })
                 this.$store.dispatch('setToken', response.data.token)
                 this.$store.dispatch('setUser', response.data.user)
                 this.$store.dispatch('setMessage', response.data.message)
+
                 this.$router.push({
                     name: 'settings'
                 })
             } catch (error) {
                 this.error = error.response.data.error
             }
+        }, 
+        debounceApiCall() {
+            clearTimeout(this.timer);
+            this.timer = setTimeout(this.displayAdress, 650)
+        },
+        displayAdress () {
+            const that = this;
+            const client = new MakeHTTPRequest();
+            const url=`https://photon.komoot.de/api/?q=${this.adress.city}&limit=3`;
+            
+            client.get(url, function(response) {
+                that.adressAutocomplete.length = 0;
+                response.features.map(result => {
+                    const entry = result.properties;
+                    
+                    //for displaying the place / city
+                    const cityName = entry.city ? entry.city : entry.name
+                    const locationItem = [cityName, entry.country, entry.state].join(',')
+                    
+                    that.adressAutocomplete.push({
+                        city: locationItem,
+                        lat: result.geometry.coordinates[0],
+                        lng: result.geometry.coordinates[1]
+                    });
+                })
+            })
         }
     }
 }
+
 </script>
 
 <style scoped>
@@ -156,5 +212,35 @@ export default {
 
 .error {
     color:red;
+}
+
+#location__input {
+    margin-bottom: 0px;
+}
+
+.suggestion__container {
+    list-style: none;
+    width:100%;
+    margin: 0;
+    padding: 0;
+    padding-left:3px;
+    text-align:left;
+}
+
+.suggestion__container a {
+    color:grey;
+    text-decoration: none;
+}
+
+.suggestion__container a:hover {
+    color:rgb(44, 54, 161);
+    transition: .3s;
+}
+
+.suggestion__container a li {
+    font-size:14px;
+    padding: 10px 0 10px 4px;
+    border-radius: 1px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 1px rgba(0, 0, 0, 0.4);
 }
 </style>
