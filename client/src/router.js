caceb9322f2cbd4425c873eb004c4bc8be6768ff
Bram <brampijper@gmail.com>
2019-10-28 13:40:54 +0100

    Removed the login route since the user can now login via a popup in the header

--- client/src/router.js
+++ client/src/router.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from './components/Register.vue'
-import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Settings from './components/Settings.vue'
-
 
 Vue.use(Router)
 
@@ -20,11 +18,6 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
     },
     {
       path: '/settings',
